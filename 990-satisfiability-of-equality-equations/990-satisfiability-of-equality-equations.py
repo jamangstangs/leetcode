b94221060fdd403d5c361768540f6515363be1ad
Song, JaeMyung <74575746+jamangstangs@users.noreply.github.com>
2022-09-26 13:48:19 +0900

    Time: 48 ms (93.97%), Space: 14.2 MB (9.51%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        
+        parent = {}
+        diff= []
+        
+        def find(x):
+            if x not in parent:
+                return x
+            else:
+                return find(parent[x])
+        
+        for i in equations:
+            a,b = i[0], i[3]
+            
+            if i[1]=="=":
+                x,y = find(a), find(b)
+                if x!=y:
+                    parent[y] = x
+            else:
+                diff.append((a,b))
+        return all(find(a)!=find(b) for a,b in diff)
+                
